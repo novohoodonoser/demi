fc011644a361805c2ce8a53b06ec84124320482b
novohoodonoser <AssAsine1990>
2020-06-08 12:50:05 +0300

    adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -1,46 +1,93 @@
-/*
-const howItWorksLink = document.getElementById('howItWorks');
-const offersLink = document.getElementById('offersLink');
-const successStoriesLink = document.getElementById('successStoriesLink');
-const successStories = document.getElementById('successStories');
-const offers = document.getElementById('offers');
-const about = document.getElementById('about');
-
-
-const howItWorks = function () {
-    about.classList.remove('invisible');
-    offers.classList.add('invisible');
-    successStories.classList.add('invisible')
-};
-const showOffers = function () {
-    offers.classList.remove('invisible');
-    successStories.classList.add('invisible');
-    about.classList.add('invisible');
-};
-
-const showSuccessStories = function () {
-    successStories.classList.remove('invisible');
-    offers.classList.add('invisible');
-    about.classList.add('invisible');
-};
-
-howItWorksLink.addEventListener('click',howItWorks);
-offersLink.addEventListener('click',showOffers);
-successStoriesLink.addEventListener('click',showSuccessStories);
-*/
-
-var slider = tns({
-    container: '.pride__container',
-    items: 2,
+let slider = tns({
+    container: '.pride__item_container',
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false
+    nav: false,
+    center: true,
+    touch: true,
+    mouseDrag: true,
+    loop: true,
+    responsive: {
+        320: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+
+    }
 });
 
-document.querySelector('.prev').addEventListener('click',function () {
+
+document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
 });
-document.querySelector('.next').addEventListener('click',function () {
+document.querySelector('.next').addEventListener('click', function () {
     slider.goTo('next');
+});
+
+$(document).ready(function () {
+// Modal
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введите {0} символа!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+
+            }
+        });
+    }
+
+    validateForms('#consultation-form');
+//
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+// Smooth scroll and pageup
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href=#up]").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top + "px"});
+        return false;
+    });
+
+    new WOW().init();
 });
